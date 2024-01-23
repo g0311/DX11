4851cce3484487f8d60abf42cb0e18a47cfb4a50
SXXK_d01 <112852343+g0311@users.noreply.github.com>
2024-01-24 01:25:57 +0900

    240124_5_E

--- DX11_5/Client/Main.cpp
+++ DX11_5/Client/Main.cpp
@@ -4,6 +4,12 @@
 #include "01. TriangleDemo.h"
 #include "02. QuadDemo.h"
 #include "03. ConstBufferDemo.h"
+#include "04. CameraDemo.h"
+#include "05. TextureDemo.h"
+#include "06. SamplingDemo.h"
+#include "07. HeightMapDemo.h"
+#include "08. NormalDemo.h"
+#include "09. MeshDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -15,7 +21,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 0.5f);
-	desc.app = make_shared<ConstBufferDemo>();
+	desc.app = make_shared<MeshDemo>();
 
 	GAME->Run(desc);
 
--- DX11_5/Libraries/Include/Engine/GeometryHelper.h
+++ DX11_5/Libraries/Include/Engine/GeometryHelper.h
@@ -5,8 +5,16 @@
 class GeometryHelper
 {
 public:
+	//사각형, 큐브, 구, 그리드, 실린더
 	static void CreateQuad(shared_ptr<Geometry<VertexColorData>> geometry, Color color);
-private:
-
+	
+	static void CreateQuad(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateCube(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateSphere(shared_ptr<Geometry<VertexTextureData>> geometry);
+	static void CreateGrid(shared_ptr<Geometry<VertexTextureData>> geometry, int32 sizeX, int32 sizeZ);
+	
+	static void CreateQuad(shared_ptr<Geometry<VertexTextureNormalData>> geometry);
+	static void CreateCube(shared_ptr<Geometry<VertexTextureNormalData>> geometry);
+	static void CreateSphere(shared_ptr<Geometry<VertexTextureNormalData>> geometry);
+	static void CreateGrid(shared_ptr<Geometry<VertexTextureNormalData>> geometry, int32 sizeX, int32 sizeZ);
 };
-
